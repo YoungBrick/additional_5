63d6d540ef9b81c8b182fd5ec3a9bea6b31e2996
YoungBrick <tidorka@gmail.com>
2018-09-24 17:24:44 +0300

    solved additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // |()|
+  let brackets  = str.split('');
+  let stack = [];
+  let config = arrayToObject(bracketsConfig); 
+  for (let i = 0; i < brackets.length; i++) {
+    let char = brackets[i];
+    if(config[char] && config[char] != char) {
+      stack.push(char);
+    } else if(config[char] == char && stack.indexOf(char) == -1) {
+      stack.push(char)
+    } else {
+      if (char != config[stack.pop()]) {
+        return false;
+      }
+    }
+  }
+  return stack.length == 0;
+}
+function arrayToObject(bracketsConfig) {
+  let config = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    config[bracketsConfig[i][0]] = bracketsConfig[i][1]
+  }
+  return config;
 }
